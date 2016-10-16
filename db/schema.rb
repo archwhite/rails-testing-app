1b24139ff6d0c08392a48d866b331b3ee82b2203
archwhite <trashopenbox@gmail.com>
2016-10-16 08:15:32 +0000

    postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161015160723) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.text     "title"
     t.text     "text"
